0bc8df2accca3c17521bde717b58b6517a442eef
jeff <jeff>
2024-06-17 17:39:25 -0400

    Fixed pause button

--- famp.py
+++ famp.py
@@ -1,7 +1,10 @@
 import time, threading, sys, os, http.server, json, urllib.parse
 
 mp = "/mount/files/Music"
-state = {"running": True, "in": False, "out": False, "vol": 30, "file": ""}
+state = {"running": True, "playing": True, 
+  "in": False, "out": False, 
+  "vol": 30, "file": "",
+  "playlist":[]}
 
 class famp(http.server.BaseHTTPRequestHandler):
 
@@ -27,11 +30,17 @@ class famp(http.server.BaseHTTPRequestHandler):
         s.ww(fl.read(), content="text/html")
     
     elif ep=="/pause":
-      os.system('echo "pause" > /tmp/famp-control')
+      if state["playing"]:
+        os.system('echo "stop" > /tmp/famp-control')
+        state["playing"] = False
+      else:
+        play(state["playlist"][0], state["pos"])
       s.ww("True")
 
     elif ep=="/next":
-      os.system('echo "pt_step 1" > /tmp/famp-control')
+      if len(state["playlist"]) > 0:
+        state["playlist"] = state["playlist"][1:]
+      play()
       s.ww("True")
 
     elif ep=="/volume":
@@ -40,8 +49,10 @@ class famp(http.server.BaseHTTPRequestHandler):
       s.ww("True")
 
     elif ep=="/play":
-      os.system(f'find "{mp + urllib.parse.unquote(qp["path"])}" | egrep -i "mp3|wma|ogg|flac" | tac > famp.playlist')
-      os.system(f'echo "loadlist {os.getcwd()}/famp.playlist" > /tmp/famp-control')
+      os.system(f'find "{mp + urllib.parse.unquote(qp["path"])}" | egrep -i "opus|mp3|wma|ogg|flac" | tac > famp.playlist')
+      with open("famp.playlist") as fl:
+        state["playlist"] = [x.strip() for x in fl.readlines() if x.strip()!=""]
+      play()
       s.ww("True")
 
     elif ep=="/list":
@@ -58,13 +69,45 @@ class famp(http.server.BaseHTTPRequestHandler):
 
     elif ep=="/state":
       s.ww(json.dumps(state))
+    
+    else:
+      s.ww("False")
+
+def play(path=None, pos=0):
+  global state
+  if path==None:
+    if len(state["playlist"]) == 0:
+      return
+    path = state["playlist"][0]
+
+  state["pos"] = pos
+  with open("famp.playlist", "w") as fl:
+    fl.write(path)
+  os.system("killall -9 mplayer")
+  if "famp-control" in os.listdir("/tmp"):
+    os.system("unlink /tmp/famp-control")
+  os.system("mkfifo /tmp/famp-control")
+  os.system(f'mplayer -slave -input file=/tmp/famp-control -msglevel all=4 -loop 0 silence.mp3 > famp.log &')
+  os.system(f'echo "volume {state["vol"]} 1" > /tmp/famp-control')
+  os.system(f'echo "loadlist famp.playlist" > /tmp/famp-control')
+  os.system(f'echo "seek {pos}" > /tmp/famp-control')
+  os.system(f'echo "loop -1" > /tmp/famp-control')
+  state["playing"] = True
 
 def get_file(state):
   while state["running"]:
-    time.sleep(0.5)
-    os.system("echo '' > famp.log")
+    os.system("ps aux | grep mplayer > famp.log")
+    with open("famp.log") as fl:
+      running = len(fl.readlines()) > 2
+    if not running:
+      if state["playing"] and len(state["playlist"]) > 0:
+        state["playlist"] = state["playlist"][1:]
+        play()
+      time.sleep(1)
+      continue
     os.system("echo 'get_property filename' > /tmp/famp-control")
     os.system("echo 'get_property volume' > /tmp/famp-control")
+    os.system("echo 'get_property time_pos' > /tmp/famp-control")
     time.sleep(0.05)
     with open("famp.log", errors='ignore') as fl:
       d = fl.readlines()
@@ -76,15 +119,17 @@ def get_file(state):
       if "ANS_volume=" in x:
         state["vol"] = x.split("ANS_volume=")[1].strip()
         got = got + 1
-      if got == 2:
+      if "ANS_time_pos=" in x:
+        state["pos"] = x.split("ANS_time_pos=")[1].strip()
+        got = got + 1
+      if got == 3:
         break
+    time.sleep(0.5)
 
 if __name__=="__main__":
-  if "famp-control" not in os.listdir("/tmp"):
-    os.system("mkfifo /tmp/famp-control")
-
-  os.system("mplayer -slave -input file=/tmp/famp-control -loop 0 -msglevel all=4 silence.mp3 > famp.log &")
-  os.system(f'echo "volume 30 1" > /tmp/famp-control')
+  if "famp-control" in os.listdir("/tmp"):
+    os.system("unlink /tmp/famp-control")
+  os.system("mkfifo /tmp/famp-control")
   os.system("amixer -c 1 set PCM 255")
   os.system("amixer -c 1 set LFE 0")
   os.system("amixer -c 1 set Center 0")
@@ -93,19 +138,18 @@ if __name__=="__main__":
   get_file_thread = threading.Thread(target=get_file, args=(state,))
   get_file_thread.start()
 
-  print("Started mplayer and set system volume to max")
+  print("Prepped for mplayer and set system volume to max")
 
   try:
     print ("Frank's Awesome Media Player started! Hit port 8000 for controls =]")
     http.server.HTTPServer(('', 8000), famp).serve_forever()
-
-  except KeyboardInterrupt:
+  except:
     print("\rBuh-bye!")
-    os.system("echo 'quit' > /tmp/famp-control &")
+    os.system("killall -9 mplayer")
     os.system("amixer -c 1 set PCM 0")
     state["running"] = False
     get_file_thread.join()
-    os.system("unlink /tmp/famp-control")
-    os.system("unlink famp.playlist")
-    os.system("unlink famp.log")
+    os.system("rm -f /tmp/famp-control")
+    os.system("rm -f famp.playlist")
+    os.system("rm -f famp.log")
     print("Quit mplayer and set system volume to zero")
